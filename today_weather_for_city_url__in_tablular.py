8361306fc8b3e0f3c60118e74bf07f76979ac99d
Krishnaraj V <128212613+KrishnarajV02@users.noreply.github.com>
2023-08-09 10:23:13 +0530

    Update today_weather_for_city_url__in_tablular.py

--- today_weather_for_city_url__in_tablular.py
+++ today_weather_for_city_url__in_tablular.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+
+city=input("Enter the city : ")
+data = requests.get("https://www.google.com/search?q=latitude+and+longitude+of+"+city)#+"&client=opera&hs=vyo&ei=WXTSZJ2VEMelhwOyqrIw&oq=lattitude+and+long+of+bangalore&gs_lp=Egxnd3Mtd2l6LXNlcnAiH2xhdHRpdHVkZSBhbmQgbG9uZyBvZiBiYW5nYWxvcmUqAggAMgUQABiiBDIFEAAYogQyBRAAGKIESLgeULcGWMoRcAF4AZABAJgBsAGgAb4FqgEDMi40uAEByAEA-AEBwgIKEAAYRxjWBBiwA8ICDBAAGA0YgAQYRhj7AcICChAAGAgYHhgNGA_CAggQABiKBRiGA8ICChAhGKABGMMEGAriAwQYACBBiAYBkAYI&sclient=gws-wiz-serp")
+data = BeautifulSoup(data.text,"html.parser")
+latlong = str(data.text)
+print(latlong)
+a=(latlong.find("Coordinates"))+11
+b = latlong.find("° N" )
+lat=latlong[a:b]
+latlong=latlong[b+5:]
+b = latlong.find("° ")
+long=latlong[:b]
+
+
 d={}
 #a = input("Enter the city url from(https://weather.com/en-IN/weather/today) : ")
-a = "https://weather.com/en-IN/weather/today/l/02144e9a9a2fe048b3ecca9df91501293f98ee712846f78a5da25ba6690fd98c"
+a = "https://weather.com/en-IN/weather/today/l/"+lat+","+long
 data = requests.get(a)
 bs = BeautifulSoup(data.text,"html.parser")
 a= bs.find_all("section",{"data-testid":"TodayWeatherModule" })
