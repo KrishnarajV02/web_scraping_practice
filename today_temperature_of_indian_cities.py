5af0087f68924ae53ee706da6e68525c92440251
Krishnaraj V <128212613+KrishnarajV02@users.noreply.github.com>
2023-08-10 20:40:11 +0530

    Update today_temperature_of_indian_cities.py

--- today_temperature_of_indian_cities.py
+++ today_temperature_of_indian_cities.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 latt, long = [], []
 for i in range(1,9): 
- data = requests.get("https://www.latlong.net/category/cities-102-15-"+str(i)+ ".html")#+"&client=opera&hs=vyo&ei=WXTSZJ2VEMelhwOyqrIw&oq=lattitude+and+long+of+bangalore&gs_lp=Egxnd3Mtd2l6LXNlcnAiH2xhdHRpdHVkZSBhbmQgbG9uZyBvZiBiYW5nYWxvcmUqAggAMgUQABiiBDIFEAAYogQyBRAAGKIESLgeULcGWMoRcAF4AZABAJgBsAGgAb4FqgEDMi40uAEByAEA-AEBwgIKEAAYRxjWBBiwA8ICDBAAGA0YgAQYRhj7AcICChAAGAgYHhgNGA_CAggQABiKBRiGA8ICChAhGKABGMMEGAriAwQYACBBiAYBkAYI&sclient=gws-wiz-serp")
+ data = requests.get("https://www.latlong.net/category/cities-102-15-"+str(i)+ ".html")
  data = BeautifulSoup(data.text, "html.parser")
  for tr in data.find_all("tr"):
     temp = tr.find_all("td")
@@ -18,16 +18,16 @@ for i in range(len(latt)):
   print(i+1 ,"rows loaded")
   weather = requests.get(weather)
   weather_bs = BeautifulSoup(weather.text,"html.parser")
-  a= weather_bs.find_all("section",{"data-testid":"TodayWeatherModule" })
+  temperature_sec= weather_bs.find_all("section",{"data-testid":"TodayWeatherModule" })
   city=weather_bs.find("h1",{"class":"CurrentConditions--location--1YWj_"}).text
-  for i in a:
-    z=i.find_all("span",{"data-testid":"TemperatureValue"})
-    morning = z[0].text
-    afternoon = z[1].text
-    evening = z[2].text
-    night = z[3].text
-  h = {"City":city,"Morning":morning,"Afternoon":afternoon,"Evenving":evening,"Night":night}
-  all_weather.append(h)
+  for temperature in temperature_sec:
+    column=temperature.find_all("span",{"data-testid":"TemperatureValue"})
+    morning = column[0].text
+    afternoon = column[1].text
+    evening = column[2].text
+    night = column[3].text
+  value = {"City":city,"Morning":morning,"Afternoon":afternoon,"Evenving":evening,"Night":night}
+  all_weather.append(value)
     
 Today_weather = pd.DataFrame(all_weather)
 Today_weather.set_index("City")
